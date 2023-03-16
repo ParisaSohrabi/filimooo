f5f52b2027b77a49e1a8c4daa2cf8452784cb886
Parisa <parisahs@gmail.com>
2023-03-15 21:03:07 -0400

    added style and navigating functionality

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 8000;
+const cors=require("cors")
 // require configuration file
 require("./config/mongoose.config");
 
 //Middleware for formating and allowing POST requests
 app.use(express.json(), express.urlencoded({ extended: true }));
+
+//Middleware for sending data back and forth to client side
+app.use(cors({origin:'http://localhost:3000'}))
 
 //import routes
 const Routes = require("./routes/movie.routes");
